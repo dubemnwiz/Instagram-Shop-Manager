7a67b23d423346a4f5fe35df0fd6db3eb5ad01b4
Dubem Nwizubo <88851578+dubemnwiz@users.noreply.github.com>
2023-09-07 10:50:44 -0400

    Created Main.java

---
+++ Main.java
@@ -0,0 +1,15 @@
+package resell;
+
+/*
+Dubem Nwizubo
+April 15, 2022
+Period 7
+Master Project
+Main
+*/
+
+public class Main {
+  public static void main(String[] args) {
+    MainGUI.main(args);
+  }
+}
